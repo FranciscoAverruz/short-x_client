49bf29e38ad3bc17f73ec0cd50c98e1a3f11da90
FAverruz <146257059+FAverruz@users.noreply.github.com>
2025-07-15 01:31:29 +0200

    feat: add AuthLayout component and apply theme detection to NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,30 +1,65 @@
+import { useEffect } from "react";
 import Button from "@atoms/Button.jsx";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import AuthLayout from "@auth/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import { IoReturnUpBack } from "react-icons/io5";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const html = document.documentElement;
+    const storedTheme = localStorage.getItem("theme");
+
+    if (storedTheme === "dark") {
+      html.classList.add("dark");
+    } else if (storedTheme === "light") {
+      html.classList.remove("dark");
+    } else {
+      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (prefersDark) {
+        html.classList.add("dark");
+      } else {
+        html.classList.remove("dark");
+      }
+    }
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
-      <motion.h1
-        className="text-6xl font-bold text-red-500"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        404
-      </motion.h1>
-      <motion.p
-        className="text-xl text-gray-600 mt-4"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        Oops! La página que buscas no existe.
-      </motion.p>
-      <Link to="/">
-        <Button className="mt-6 px-6 py-3 text-lg">Volver al inicio</Button>
-      </Link>
-    </div>
+    <main className="flex w-screen h-screen items-center justify-center bg-light-gradient dark:bg-dark-gradient">
+
+      <AuthLayout
+        classNameAL="flex px-20 text-center"
+        formContent={
+          <div className="flex flex-col w-full">
+            <motion.h1
+              className="text-6xl font-bold text-red-500"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              404
+            </motion.h1>
+            <motion.p
+              className="text-xl text-light-Title dark:text-dark-Title mt-4"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              Oops! La página que buscas no existe.
+            </motion.p>
+            <Button
+              label="Volver al inicio"
+              variant="primary"
+              onClick={() => navigate("/")}
+              icon={IoReturnUpBack}
+              className="font-bold mt-5 md:ml-1"
+            />
+          </div>
+        }
+      />
+    </main>
   );
 };
 
