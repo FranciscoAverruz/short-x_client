14d85b195b6143248794186382e569828b47aaaf
FAverruz <146257059+FAverruz@users.noreply.github.com>
2025-07-15 01:07:45 +0200

    feat(change-password): support both profile change and forgot password flows

--- src/features/dashboard/components/myAccount/ChangePassword.jsx
+++ src/features/dashboard/components/myAccount/ChangePassword.jsx
@@ -10,51 +10,63 @@ import { API_URL } from "@src/Env.jsx";
 import { TbCancel } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "@context/AuthContext";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const ChangePassword = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const token = queryParams.get("token");
+  const isResetMode = Boolean(token);
   const { userId } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const [passwordChecked, setPasswordChecked] = useState(false);
-  const [formData, setFormData] = useState({
+  const [passwords, setPasswords] = useState({
     currentPassword: "",
     newPassword: "",
     confirmPassword: "",
   });
+  const { currentPassword, newPassword, confirmPassword } = passwords;
+  const passwordInfo = {
+    length: newPassword.length,
+    match: newPassword.length > 0 && newPassword === confirmPassword,
+  };
   const navigate = useNavigate();
   const authAxios = useAuthAxios();
 
   const handleChange = (e) => {
     const { id, value } = e.target;
-    setFormData({
-      ...formData,
+    setPasswords({
+      ...passwords,
       [id]: value,
     });
   };
 
   useEffect(() => {
-    if (formData.newPassword.length > 0) {
+    if (newPassword.length > 0) {
       setPasswordChecked(true);
     }
-  }, [formData.newPassword]);
+  }, [newPassword]);
 
+  // ************************************
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{5,}$/;
-    if (!passwordRegex.test(formData.newPassword || formData.confirmPassword)) {
+    if (!passwordRegex.test(newPassword || confirmPassword)) {
       toast.warning("La contraseña no cumple con los requisitos.");
       return;
     }
 
-    if (formData.newPassword !== formData.confirmPassword) {
+    if (newPassword !== confirmPassword) {
       toast.warning("Las contraseñas no coinciden");
       return;
     }
 
     if (
-      !formData.currentPassword ||
-      !formData.newPassword ||
-      !formData.confirmPassword
+      (!isResetMode && !currentPassword) ||
+      !newPassword ||
+      !confirmPassword
     ) {
       toast.error("Por favor, completa todos los campos.");
       return;
@@ -62,19 +74,30 @@ const ChangePassword = () => {
 
     setLoading(true);
     try {
-      const response = await authAxios.put(
-        `${API_URL}/user/${userId}/change-password`,
-        formData
+      let response;
+
+      if (isResetMode) {
+        response = await axios.post(`${API_URL}/reset-password`, {
+          token,
+          newPassword,
+        });
+      } else {
+        response = await authAxios.put(
+          `${API_URL}/user/${userId}/change-password`,
+          passwords
+        );
+      }
+      toast.success(
+        response.data.message || "Contraseña cambiada correctamente"
       );
-      toast.success(response.data.message);
-      setFormData({
+      setPasswords({
         currentPassword: "",
         newPassword: "",
         confirmPassword: "",
       });
       setPasswordChecked(false);
       setTimeout(() => {
-        navigate("/dashboard/myaccount");
+        navigate(isResetMode ? "/login" : "/dashboard/myaccount");
       }, 1000);
     } catch (error) {
       toast.error(
@@ -84,43 +107,52 @@ const ChangePassword = () => {
       setLoading(false);
     }
   };
-
+  // ************************************
   return (
-    <section className="max-w-md mx-auto mt-10 p-8 grlContainer shadow-lg rounded-lg">
-      <h2 className="title text-xl font-semibold text-center mb-4">
-        Cambiar Contraseña
-      </h2>
+    <section className="flex w-full h-[80dvh] items-center justify-center">
+      <aside className="max-w-md mx-auto mt-10 p-8 grlContainer shadow-lg rounded-lg w-full">
+        <h2 className="title text-xl font-semibold text-center mb-4">
+          Cambiar Contraseña
+        </h2>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <PasswordInput
-          label="Contraseña actual"
-          id="currentPassword"
-          value={formData.currentPassword}
-          onChange={handleChange}
-          placeholder="Ingresa tu contraseña actual"
-          required
-        />
-        <PasswordValidation
-          newPassword={formData.newPassword}
-          confirmPassword={formData.confirmPassword}
-          onPasswordChange={handleChange}
-          onConfirmPasswordChange={handleChange}
-          passwordChecked={passwordChecked}
-        />
-        <aside className="flex flex-row justify-between">
-          <Button
-            label="Cancelar"
-            variant="secondary"
-            icon={TbCancel}
-            onClick={() => navigate("/dashboard/myaccount")}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {!isResetMode && (
+            <PasswordInput
+              label="Contraseña actual"
+              id="currentPassword"
+              value={currentPassword}
+              onChange={handleChange}
+              placeholder="Ingresa tu contraseña actual"
+              required
+            />
+          )}
+
+          <PasswordValidation
+            newPassword={newPassword}
+            confirmPassword={confirmPassword}
+            onPasswordChange={handleChange}
+            onConfirmPasswordChange={handleChange}
+            passwordChecked={passwordChecked}
+            passwordInfo={passwordInfo}
           />
-          <SubmitButton
-            type="submit"
-            disabled={loading}
-            label={loading ? "Cambiando..." : "Cambiar Contraseña"}
-          />
-        </aside>
-      </form>
+
+          <aside className="flex flex-row justify-between">
+            <Button
+              label="Cancelar"
+              variant="secondary"
+              icon={TbCancel}
+              onClick={() =>
+                navigate(isResetMode ? "/login" : "/dashboard/myaccount")
+              }
+            />
+            <SubmitButton
+              type="submit"
+              disabled={loading}
+              label={loading ? "Cambiando..." : "Cambiar Contraseña"}
+            />
+          </aside>
+        </form>
+      </aside>
     </section>
   );
 };
