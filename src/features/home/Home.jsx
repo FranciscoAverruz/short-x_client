f8e4f7c367ddcaa36ca71859d4eba191768f98a5
FAverruz <146257059+FAverruz@users.noreply.github.com>
2025-07-15 01:10:28 +0200

    feat(home): wrap sections with RevealOnScroll for scroll-based animations

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -4,41 +4,48 @@ import FAQSection from "@homeSections/FAQSection.jsx";
 import HeroSection from "@homeSections/HeroSection.jsx";
 import PricingSection from "@homeSections/PricingSection.jsx";
 import BenefitsSection from "@homeSections/BenefitsSection.jsx";
+import RevealOnScroll from "@common/RevealOnScroll.jsx";
 // import FinalCTA from "@homeSections/FinalCTA.jsx";
 
 const Home = () => {
   return (
     <main className="w-screen">
       {/* Hero Section */}
-      <section className="relative w-full h-auto flex items-center py-14 justify-center">
-        <HeroSection />
-      </section>
+      <RevealOnScroll>
+        <section className="relative w-full h-full flex items-center py-12 mt-10 justify-center">
+          <HeroSection />
+        </section>
+      </RevealOnScroll>
 
       {/* Benefits */}
-      <section className="w-screen h-fit flex items-center justify-center">
-        <article className="p-10 rounded-3xl bg-gradient-to-b from-transparent/30 dark:from-dark-sectionBg to-transparent via-light-inputBg/20">
-          <BenefitsSection />
-        </article>
-      </section>
+      <RevealOnScroll>
+        <section className="relative w-screen h-fit flex items-center justify-center">
+          <article className="p-10 rounded-3xl bg-gradient-to-b from-transparent/30 dark:from-dark-sectionBg to-transparent via-light-inputBg/20">
+            <BenefitsSection />
+          </article>
+        </section>
+      </RevealOnScroll>
 
       {/* pricing */}
-      <section className="relative w-full h-fit flex items-center justify-center px-5 py-20">
-        <article className="md:w-4/5">
-          <PricingSection />
-        </article>
-      </section>
+        <section className="relative w-full h-fit flex items-center justify-center px-5 py-20">
+          <article className="md:w-4/5">
+            <PricingSection />
+          </article>
+        </section>
 
       {/* FAQ */}
-      <section className="h-fit">
-        <FAQSection />
-      </section>
+        <section className="h-fit ">
+          <FAQSection />
+        </section>
 
       {/* CTA */}
-      {/* <section className="relative w-full min-h-[55vh] flex items-center justify-center bg-light-primary/60">
+      {/*<RevealOnScroll>
+      <section className="relative w-full min-h-[55vh] flex items-center justify-center bg-light-primary/60">
         <article className="pb-5">
           <FinalCTA />
         </article>
-      </section> */}
+      </section>
+      </RevealOnScroll> */}
     </main>
   );
 };
