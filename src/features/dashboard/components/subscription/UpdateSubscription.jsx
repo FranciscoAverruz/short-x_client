afe8fc7f7e46cfe43aa447fafe5e69ac69c5c16d
FAverruz <146257059+FAverruz@users.noreply.github.com>
2025-08-22 00:24:37 +0200

    style: switch layout from flex to grid, replace <span> with <section>, and apply Prettier formatting

--- src/features/dashboard/components/subscription/UpdateSubscription.jsx
+++ src/features/dashboard/components/subscription/UpdateSubscription.jsx
@@ -104,138 +104,145 @@ const UpdateSubscription = ({ currentPlan = "free_monthly" }) => {
 
   return (
     <>
-    <AnimatePresence>
-      <motion.main
-        key="updateSubscription"
-        initial={{ opacity: 0, y: -10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
-        className="grlContainer flex flex-col md:flex-row gap-5 lg:gap-10 p-6 w-full items-center"
-      >
-        <aside className="flex flex-col justify-between h-full w-full md:w-full">
-          <h2 className="text-xl subTitle1">Cambiar Plan</h2>
-          <article className="flex flex-col md:flex-row lg:flex-col md:gap-5 lg:gap-0 md:w-full md:justify-center">
-            <span className="relative flex flex-row lg:flex-row items-end p-0 w-full">
-            <Input
-              label={"Nuevo Plan"}
-              type="text"
-              id="newPlan"
-              value={newPlan}
-              className="rounded-r-none w-full text-transparent "
-              readOnly
-            />
-            <span className="absolute left-4 top-10 transform text-amber-500 font-semibold text-lg">
-              <PlanLabel plan={newPlan} /> {/* this will show over id="newPlan" */}
-            </span>
-              <Button
-                label="Cambiar"
-                onClick={() => setModalOpen(true)}
-                className="h-[2.6rem] m-0 px-4 md:px-2 text-xs rounded-none rounded-r-lg inputStyle top-0 hover:brightness-125"
-                variant="secondary"
+      <AnimatePresence>
+        <motion.main
+          key="updateSubscription"
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+          className="grlContainer flex flex-col md:flex-row gap-5 lg:gap-10 p-6 w-full items-center"
+        >
+          <aside className="flex flex-col justify-between h-full w-full">
+            <h2 className="text-xl subTitle1">Cambiar Plan</h2>
+            <article className="flex flex-col md:flex-row lg:flex-col md:gap-5 lg:gap-0 md:w-full md:justify-center">
+              {/* <section className="relative flex flex-row lg:flex-row items-end p-0 w-full bg-pink-400"> */}
+              <section className="relative grid grid-cols-3 items-end">
+                <span className="col-span-2">
+                  <Input
+                  label={"Nuevo Plan"}
+                  type="text"
+                  id="newPlan"
+                  value={newPlan}
+                  className="rounded-r-none w-full text-transparent"
+                  style={{ textIndent: "-9999px" }}
+                  readOnly
+                />
+                </span>
+                <span className="absolute left-4 top-10 transform text-amber-500 font-semibold text-lg col-span-2">
+                  <PlanLabel plan={newPlan} /> {/* this will show over id="newPlan" */}
+                </span>
+                <Button
+                  label="Cambiar"
+                  onClick={() => setModalOpen(true)}
+                  className="h-[2.6rem] m-0 px-4 md:px-2 text-xs rounded-none rounded-r-lg inputStyle top-0 hover:brightness-125 col-span-1"
+                  variant="secondary"
+                />
+              </section>
+              <SubmitButton
+                label="Actualizar"
+                variant={
+                  subscription?.status === "pending" ? "secondary" : "primary"
+                }
+                onClick={fetchUpcomingPayment}
+                disabled={loading}
+                className="w-full mt-3 md:h-11 md:self-end lg:h-auto lg:self-auto"
               />
-            </span>
-            <SubmitButton
-              label="Actualizar"
-              variant={
-                subscription?.status === "pending" ? "secondary" : "primary"
-              }
-              onClick={fetchUpcomingPayment}
-              disabled={loading}
-              className="w-full mt-3 md:h-11 md:self-end lg:h-auto lg:self-auto"
+            </article>
+          </aside>
+
+          <PricingModal
+            isOpen={modalOpen}
+            closeModal={() => setModalOpen(false)}
+            handlePlanSelect={handlePlanSelect}
+          />
+          {loading ? (
+            <Loader
+              type="spinner"
+              className="fixed inset-0 w-full h-full bg-black bg-opacity-60"
             />
-          </article>
-        </aside>
-
-        <PricingModal
-          isOpen={modalOpen}
-          closeModal={() => setModalOpen(false)}
-          handlePlanSelect={handlePlanSelect}
-        />
-        {loading ? (
-          <Loader
-            type="spinner"
-            className="fixed inset-0 w-full h-full bg-black bg-opacity-60"
-          />
-        ) : (
-          <ConfirmModal
-            open={confirmOpen}
-            onClose={() => setConfirmOpen(false)}
-            title="Confirmar actualización de suscripción"
-            content={
-              newPlan === "free_monthly" || newPlan === "free_annual" ? (
-                <aside className="flex flex-col justify-center items-center grlTxt">
-                  <p>
-                    El cambio de tu plan
-                    <strong className="smart text-xl mx-2">
-                      <PlanLabel plan={subscription.plan} />
-                    </strong>
-                    a plan <strong className="smart text-xl mx-2">Free</strong>,
-                    se completará el dia:
-                  </p>
-
-                  <strong className="smart text-xl mb-5">
-                    {FormatDate(subscription.renewalDate)}
-                  </strong>
-
-                  <p>podrás continuar usando tu plan actual hasta esa fecha</p>
-                </aside>
-              ) : upcomingPayment ? (
-                <aside className="grlTxt">
-                  <span className="flex flex-col md:flex-row items-center md:items-baseline md:gap-2 w-full mb-8 md:mb-0">
-                    <p className="subTitle2 text-base">Próximo pago:</p>
-                    <p className="text-2xl">
-                      {upcomingPayment.totalAmountDue}{" "}
-                      {upcomingPayment.currency}
+          ) : (
+            <ConfirmModal
+              open={confirmOpen}
+              onClose={() => setConfirmOpen(false)}
+              title="Confirmar actualización de suscripción"
+              content={
+                newPlan === "free_monthly" || newPlan === "free_annual" ? (
+                  <aside className="flex flex-col justify-center items-center grlTxt">
+                    <p>
+                      El cambio de tu plan
+                      <strong className="smart text-xl mx-2">
+                        <PlanLabel plan={subscription.plan} />
+                      </strong>
+                      a plan 
+                      <strong className="smart text-xl mx-2">Free</strong>, se
+                      completará el dia:
                     </p>
-                  </span>
-                  <span className="flex flex-row items-baseline gap-2 w-full mb-3">
-                    <p className="subTitle2 text-base">Fecha:</p>
-                    {FormatDate(upcomingPayment.date)}
-                  </span>
-                  <hr className="divider" />
-                  <ul className="px-0 md:px-3">
-                    {upcomingPayment.lineItems.map((item, index) => (
-                      <li
-                        key={index}
-                        className={`flex flex-col md:flex-row md:justify-between w-full px-2 ${
-                          index % 2 === 0
-                            ? "bg-light-inputText/20 dark:bg-dark-inputText/10"
-                            : "bg"
-                        }`}
-                      >
-                        <span className="smart text-base font-medium md:mr-10">
-                          {item.description}:
-                        </span>
-                        <span className="flex justify-end">
-                          {item.amount} {upcomingPayment.currency}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </aside>
-              ) : (
-                <Loader type="spinner" className="w-full h-full" />
-              )
-            }
-            onConfirm={handleUpdateSubscription}
-            loading={loading}
-          />
-        )}
-      </motion.main>
-    </AnimatePresence>
-            <PremiumModal
-            isOpen={isPremiumOpen}
-            onClose={() => {
-              setIsPremiumOpen(false);
-              navigate("/dashboard/subscription");
-            }}
-            handlePlanSelect={(planType, billing) => {
-              setNewPlan(`${planType}_${billing}`);
-              setIsPremiumOpen(false);
-              navigate("/dashboard/subscription");
-            }}
-          />
-          </>
+
+                    <strong className="smart text-xl mb-5">
+                      {FormatDate(subscription.renewalDate)}
+                    </strong>
+
+                    <p>
+                      podrás continuar usando tu plan actual hasta esa fecha
+                    </p>
+                  </aside>
+                ) : upcomingPayment ? (
+                  <aside className="grlTxt">
+                    <span className="flex flex-col md:flex-row items-center md:items-baseline md:gap-2 w-full mb-8 md:mb-0">
+                      <p className="subTitle2 text-base">Próximo pago:</p>
+                      <p className="text-2xl">
+                        {upcomingPayment.totalAmountDue}{" "}
+                        {upcomingPayment.currency}
+                      </p>
+                    </span>
+                    <span className="flex flex-row items-baseline gap-2 w-full mb-3">
+                      <p className="subTitle2 text-base">Fecha:</p>
+                      {FormatDate(upcomingPayment.date)}
+                    </span>
+                    <hr className="divider" />
+                    <ul className="px-0 md:px-3">
+                      {upcomingPayment.lineItems.map((item, index) => (
+                        <li
+                          key={index}
+                          className={`flex flex-col md:flex-row md:justify-between w-full px-2 ${
+                            index % 2 === 0
+                              ? "bg-light-inputText/20 dark:bg-dark-inputText/10"
+                              : "bg"
+                          }`}
+                        >
+                          <span className="smart text-base font-medium md:mr-10">
+                            {item.description}:
+                          </span>
+                          <span className="flex justify-end">
+                            {item.amount} {upcomingPayment.currency}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </aside>
+                ) : (
+                  <Loader type="spinner" className="w-full h-full" />
+                )
+              }
+              onConfirm={handleUpdateSubscription}
+              loading={loading}
+            />
+          )}
+        </motion.main>
+      </AnimatePresence>
+      <PremiumModal
+        isOpen={isPremiumOpen}
+        onClose={() => {
+          setIsPremiumOpen(false);
+          navigate("/dashboard/subscription");
+        }}
+        handlePlanSelect={(planType, billing) => {
+          setNewPlan(`${planType}_${billing}`);
+          setIsPremiumOpen(false);
+          navigate("/dashboard/subscription");
+        }}
+      />
+    </>
   );
 };
 
